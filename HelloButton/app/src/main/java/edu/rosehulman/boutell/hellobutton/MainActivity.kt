6cf3c24bd0595689cd4394a8c3c63cfd69d15d34
Matt Boutell <boutell@gmail.com>
2020-07-01 17:38:00 -0400

    Update to new gradle and library versions, plus try out coroutines (unsuccessful but documented).

--- HelloButton/app/src/main/java/edu/rosehulman/boutell/hellobutton/MainActivity.kt
+++ HelloButton/app/src/main/java/edu/rosehulman/boutell/hellobutton/MainActivity.kt
@@ -1,9 +1,15 @@
 package edu.rosehulman.boutell.hellobutton
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
+import androidx.core.content.ContextCompat
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +23,37 @@ class MainActivity : AppCompatActivity() {
             numClicks++
             updateView()
         }
+
+        // Launch firebase stuff on another thread.
+        // When it finishes, call another method.
+        // How do we know when it is done?
+
+        // testCoroutine()
+        testAnko()
+
+
+    }
+
+    private fun testAnko() {
+        doAsync{
+//            val result = "17"
+            val result = URL("https://httpbin.org/get").readText()
+            uiThread {
+                toast(result)
+            }
+        }
+    }
+
+    private fun testCoroutine() {
+        GlobalScope.launch {
+            // Does something on a different thread.
+            // Learn the other library too.
+            // launch new coroutine in background and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+            Log.d("COROUT", "World!") // print after delay
+        }
+        Log.d("COROUT", "Hello,") // main thread continues while coroutine is delayed
+        Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
     }
 
     fun updateView() {
